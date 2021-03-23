59afe16e9f649ecf7a42d1091849dd871b79b33a
chris interiano <cinteriano@live.com>
2021-03-22 22:18:57 -0700

    working on schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,18 @@ CREATE DATABASE employment_db;
 
 
 USE employment_db;
+
+CREATE TABLE people (
+  
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  
+  name VARCHAR(30) NOT NULL,
+  
+  employee_job BOOLEAN NOT NULL,
+
+  employee_name VARCHAR(30),
+  
+  employee_age INTEGER(10),
+  
+  PRIMARY KEY (id)
+);

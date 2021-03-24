f4dd2ad73efe25a902cbb3385cee1bef8fd6054f
chris interiano <cinteriano@live.com>
2021-03-23 17:22:50 -0700

    working on tables

--- schema.sql
+++ schema.sql
@@ -17,6 +17,8 @@ CREATE TABLE role (
   
   id INT PRIMARY KEY,
   title VARCHAR(30)
+  salary DECIMAL
+  department_id INT
   
   
 );
@@ -24,7 +26,10 @@ CREATE TABLE role (
 CREATE TABLE employee (
   
   id INT PRIMARY KEY,
-  
+  first_name VARCHAR(30)
+  last_name VARCHAR(30)
+  role_id INT
+  manager_id INT
   
 );
 
